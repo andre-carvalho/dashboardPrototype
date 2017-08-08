b67913ee37961a28af035a750051a836673cec5a
andre.carvalho <andre.carvalho@funcate.org.br>
2017-08-08 17:40:24 -0300

    First beta version, lack the graph titles, the filter display and some cosmetic adjusts.

--- js/lineComposite.js
+++ js/lineComposite.js
@@ -1,11 +1,23 @@
-var focusChart = dc.seriesChart("#test");
-var overviewChart = dc.seriesChart("#test-overview");
-var ringTotalizedByState = dc.pieChart("#chart-by-state");
-var ringTotalizedByClass = dc.pieChart("#chart-by-class");
-var barAreaByYear = dc.barChart("#chart-by-year");
-var ndx, temporalDimension, yearDimension, monthDimension, ufDimension, classDimension;
+var focusChart = dc.seriesChart("#test", "agrega");
+var overviewChart = dc.seriesChart("#test-overview", "agrega");
+var ringTotalizedByState = dc.pieChart("#chart-by-state", "filtra");
+var ringTotalizedByClass = dc.pieChart("#chart-by-class", "filtra");
+var barAreaByYear = dc.barChart("#chart-by-year", "filtra");
+var ndx0,ndx1, temporalDimension, yearDimension, monthDimension, ufDimension, classDimension;
 var areaGroup, ufGroup, classGroup, yearGroup;
 var utils = {
+		/*
+		 * Remove numeric values less than 1e-6
+		 */
+		snapToZero:function(sourceGroup) {
+			return {
+				all:function () {
+					return sourceGroup.all().map(function(d) {
+						return {key:d.key,value:( (Math.abs(d.value)<1e-6) ? 0 : d.value )};
+					});
+				}
+			};
+		},
 		rangesEqual: function (range1, range2) {
 	        if (!range1 && !range2) {
 	            return true;
@@ -282,19 +294,33 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 		if(month >=8 && month<=12) {
 			year = "20"+year+"/20"+(year+1);
 		}
-		o.push({Year:year,Month:month,Area:+((fet.properties.ar).toFixed(2)),uf:fet.properties.uf,cl:fet.properties.cl});
+		o.push({Year:year,Month:month,Area:+((fet.properties.ar).toFixed(1)),uf:fet.properties.uf,cl:fet.properties.cl});
 	}
 	data = o;
 
-	var w=window.innerWidth - 100, h=window.innerHeight * 0.5;
+	var w=window.innerWidth - 100, h=window.innerHeight * 0.4;
 	
-	ndx = crossfilter(data);
-	temporalDimension = ndx.dimension(function(d) {
+	ndx0 = crossfilter(data);
+	ndx1 = crossfilter(data);
+	temporalDimension = ndx0.dimension(function(d) {
 		var m=utils.fakeMonths(d.Month);
 		return [d.Year, m];
 	});
-	
-	yearDimension = ndx.dimension(function(d) {
+	areaGroup = temporalDimension.group().reduceSum(function(d) {
+		return d.Area;
+	});
+
+	yearDimension0 = ndx0.dimension(function(d) {
+		return d.Year;
+	});
+	classDimension0 = ndx0.dimension(function(d) {
+		return d.cl;
+	});
+	ufDimension0 = ndx0.dimension(function(d) {
+		return d.uf;
+	});	
+
+	yearDimension = ndx1.dimension(function(d) {
 		return d.Year;
 	});
 
@@ -302,11 +328,13 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 		return d.Area;
 	});
 	
-	monthDimension = ndx.dimension(function(d) {
-		return d.Month;
+	monthDimension = ndx1.dimension(function(d) {
+		var m=utils.fakeMonths(d.Month);
+		return m;
+		//return d.Month;
 	});
 	
-	ufDimension = ndx.dimension(function(d) {
+	ufDimension = ndx1.dimension(function(d) {
 		return d.uf;
 	});
 
@@ -314,15 +342,11 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 		return d.Area;
 	});
 	
-	classDimension = ndx.dimension(function(d) {
+	classDimension = ndx1.dimension(function(d) {
 		return d.cl;
 	});
 
 	classGroup = classDimension.group().reduceSum(function(d) {
-		return d.Area;
-	});
-	
-	areaGroup = temporalDimension.group().reduceSum(function(d) {
 		return d.Area;
 	});
 
@@ -350,11 +374,13 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 		.renderHorizontalGridLines(true)
 		.renderVerticalGridLines(true)
 		.brushOn(false)
-		.yAxisLabel("Total Area km²")
+		.yAxisLabel("Total Area (km²)")
 		.xAxisLabel("Months")
 		.elasticY(true)
+		.yAxisPadding('10%')
 		.dimension(temporalDimension)
 		.group(areaGroup)
+		//.group(utils.removeLittlestValues(areaGroup))
 		//.brushOn(true)// seleciona área mas não aplica zoom 
 		//.mouseZoomable(true)
 		.rangeChart(overviewChart)
@@ -369,7 +395,6 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 			return d.key[1];
 		})
 		.valueAccessor(function(d) {
-			//console.log(d.key[1] + ":" + d.value);
 			return Math.abs(+(d.value.toFixed(2)));
 		})
 		.legend(dc.legend().x(w-200).y(10).itemHeight(15).gap(5).horizontal(1).legendWidth(200).itemWidth(80));
@@ -380,27 +405,15 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 	focusChart.xAxis().tickFormat(function(d) {
 		return utils.xaxis(d);
 	});
-	/*
-	focusChart.on('renderlet', function(chart) {
-		chart.selectAll("circle")
-		.style("stroke", "gray")
-        .style("fill", "transparent")
-        .attr("r", 25).append("text")
-	    .attr("text", function(d) {
-	    	return d.y + " Km²";
-	    });
-		
-	    chart.selectAll("circle")
-	    .append("text")
-	    .attr("text", function(d) { return d.value })
-	});
-	*/
+
 	focusChart.margins().left += 40;
 	focusChart.margins().top += 30;
 	
-	/*focusChart.on('filtered', function(chart) {
-	    console.log(chart.filter()[0] + "-" + chart.filter()[1]);
-	});*/
+	focusChart.on('filtered', function(chart) {
+		monthDimension.filterRange([chart.filter()[0], (chart.filter()[1]+1) ]);
+		dc.redrawAll("filtra");
+	    //console.log(chart.filter()[0] + "-" + chart.filter()[1]);
+	});
 
 	focusChart.colorAccessor(function(d) {
 		var i=0,l=barColors.length;
@@ -423,6 +436,8 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 		                dc.events.trigger(function () {
 		                    overviewChart.focusChart().x().domain(overviewChart.focusChart().xOriginalDomain());
 		                    overviewChart.focusChart().redraw();
+		                    monthDimension.filterAll();
+		                    dc.redrawAll("filtra");
 		                });
 		            } else if (!utils.rangesEqual(chart.filter(), overviewChart.focusChart().filter())) {
 		                dc.events.trigger(function () {
@@ -430,6 +445,7 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 		                });
 		            }
 		        });
+
 		    return chart;
 	    })
 	    .x(d3.scale.linear().domain([8,19]))
@@ -438,6 +454,7 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 	    .clipPadding(10)
 	    .dimension(temporalDimension)
 		.group(areaGroup)
+		//.group(utils.removeLittlestValues(areaGroup))
 	    .seriesAccessor(function(d) {
 			return d.key[0];
 		})
@@ -453,9 +470,64 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 		return d3.format(',d')(d);
 	});
 	overviewChart.xAxis().tickFormat(function(d) {
+		//return d;
 		return utils.xaxis(d);
 	});
 
+	overviewChart.round(
+		function round(v) {
+			var v1 = parseInt(v);
+			return v1;
+    	}
+	);
+
+	overviewChart.on('filtered', function(chart, filter) {
+		var rangeFilter = chart.filter();
+		var focusFilter = chart.focusChart().filter();
+		if (focusFilter && !rangeFilter) {
+			dc.events.trigger(function () {
+				console.log("trigger to overviewChart.renderlet");
+				//chart.focusChart().replaceFilter(rangeFilter);
+			});
+		}
+		/*
+		if(chart.hasFilter()) {
+			if(!chart.filters()[0].isFiltered()) {
+				console.log("NoFiltered");
+			}else{
+				console.log("Filtered");
+			}
+		}
+		*/
+	});
+
+	/*
+	focusChart.on('renderlet', function(chart) {
+    // smooth the rendering through event throttling
+		dc.events.trigger(function(){
+			if(!overviewChart.filter()) {
+				console.log('NoFiltered');
+			}else{
+				console.log('Filtered');
+			}
+		});
+	});*/
+
+/*
+	overviewChart
+		.on('renderlet', function(chart,filter) {
+			console.log('renderlet:'+(chart.hasFilter()?"hasFilter":"NoFilter"));
+			
+			if(chart.hasFilter()) {
+				chart.needRedraw=true;
+				return;
+			}
+			if(chart.needRedraw) {
+				chart.needRedraw=false;
+				dc.redrawAll("filtra");
+			}
+		});
+*/
 	ringTotalizedByState
 		.height(350)
 		.width(550)
@@ -463,6 +535,10 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 		.externalRadiusPadding(60)
 		.dimension(ufDimension)
 		.group(ufGroup)
+		.title(function(d) {
+			return "Área: " + Math.abs(+(d.value.toFixed(2))) + " Km²";
+		})
+		//.group(utils.snapToZero(ufGroup))
 		//.colors(d3.scale.category20())
 		.ordinalColors(["#0000FF","#56B2EA","#00FFFF","#F8B700","#78CC00","#FF00FF","#FF0000","#FFFF00","#00FF00"])
 		.legend(dc.legend());
@@ -474,26 +550,32 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 		.innerRadius(25)
 		.externalRadiusPadding(60)
 		.dimension(classDimension)
-		.group(classGroup)
+		//.group(classGroup)
+		.group(utils.snapToZero(classGroup))
+		.title(function(d) {
+			return "Área: " + Math.abs(+(d.value.toFixed(2))) + " Km²";
+		})
 		//.colors(d3.scale.category20())
 		.ordinalColors(["#0000FF","#56B2EA","#00FFFF","#F8B700","#78CC00","#FF00FF","#FF0000","#FFFF00","#00FF00"])
 		//.legend(dc.legend().x(0).y(0).itemHeight(10).gap(2).horizontal(1).legendWidth(450).itemWidth(130));
 		.legend(dc.legend());
 
 	barAreaByYear
-		.height(300)
-		.width(350)
+		.height(350)
+		.width(400)
 		.yAxisLabel("Área (Km²)")
 		.xAxisLabel("Year (PRODES)")
 		.dimension(yearDimension)
-		.group(yearGroup)
+		.group(utils.snapToZero(yearGroup))
+		.title(function(d) {
+			return "Área: " + Math.abs(+(d.value.toFixed(2))) + " Km²";
+		})
 		.elasticY(true)
 		.x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .barPadding(0.2)
 		.outerPadding(0.1)
 		.renderHorizontalGridLines(true)
-		//.ordinalColors(['#FF8D00','#008DFF'])
 		.colorAccessor(function(d) {
 			var i=0,l=barColors.length;
 			while(i<l){
@@ -503,24 +585,67 @@ d3.json("data/deter_month_dashboard1.json", function(error, data) {
 				i++;
 			}
 		});
-		//.colors(d3.scale.ordinal().range(['#FF8D00']));
-		
-/*
-		.colors( ['rgb(215,48,39)', 'rgb(244,109,67)', 'rgb(253,174,97)', 'rgb(254,224,144)' ] )
-		.colorDomain ([0,3])
-		.colorAccessor(function(d, i){
-			  if(d.key == '2015/2016')
-				 return 3;
-			  else if(d.key == '2015/2016')
-				return 2;
-			  else return 1;
-		});
-*/
-	barAreaByYear.margins().left += 50;
 
-/*	focusChart.on('pretransition', function(chart) {
-		utils.setControls();
-	});*/
-	//utils.setControls();
-	dc.renderAll();
+	barAreaByYear.margins().left += 30;
+
+	dc.chartRegistry.list("filtra").forEach(function(c,i){
+		c.on('filtered', function(chart, filter) {
+			var filters = chart.filters();
+
+			if(chart.anchorName()=="chart-by-year"){
+				if(!filters.length) {
+					yearDimension0.filterAll();
+				}else {
+					yearDimension0.filterFunction(function (d) {
+						for (var i = 0; i < filters.length; i++) {
+							var f = filters[i];
+							if (f.isFiltered && f.isFiltered(d)) {
+								return true;
+							} else if (f <= d && f >= d) {
+								return true;
+							}
+						}
+						return false;
+					});
+				}
+			}
+			if(chart.anchorName()=="chart-by-class"){
+				if(!filters.length) {
+					classDimension0.filterAll();
+				}else {
+					classDimension0.filterFunction(function (d) {
+						for (var i = 0; i < filters.length; i++) {
+							var f = filters[i];
+							if (f.isFiltered && f.isFiltered(d)) {
+								return true;
+							} else if (f <= d && f >= d) {
+								return true;
+							}
+						}
+						return false;
+					});
+				}
+			}
+			if(chart.anchorName()=="chart-by-state"){
+				if(!filters.length) {
+					ufDimension0.filterAll();
+				}else {
+					ufDimension0.filterFunction(function (d) {
+						for (var i = 0; i < filters.length; i++) {
+							var f = filters[i];
+							if (f.isFiltered && f.isFiltered(d)) {
+								return true;
+							} else if (f <= d && f >= d) {
+								return true;
+							}
+						}
+						return false;
+					});
+				}
+			}
+			dc.redrawAll("agrega");
+		});
+	});
+	dc.renderAll("agrega");
+	dc.renderAll("filtra");
 });
